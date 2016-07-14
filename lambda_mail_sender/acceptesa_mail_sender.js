0a8e72e43d45c44e7c25af6dd5af653d18476d2c
celeron1ghz <celeron1ghz@gmail.com>
2016-07-14 20:43:48 +0900

    change to use promise()

--- lambda_mail_sender/acceptesa_mail_sender.js
+++ lambda_mail_sender/acceptesa_mail_sender.js
@@ -8,29 +8,20 @@ let ses = new aws.SES();
 exports.handler = (event, context, callback) => {
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-    const params = { Bucket: bucket, Key: key };
-    
-    s3.getObject(params, (err, data) => {
-        if (err) {
+
+    s3.getObject({ Bucket: bucket, Key: key }).promise()
+        .then(data => {
+            //console.log(data.Body.toString());
+            return ses.sendRawEmail({ RawMessage: { Data: data.Body.toString() } }).promise();
+        })
+        .then(data => {
+            //console.log("send ok");
+            callback(null, data);
+        })
+        .catch(err => {
             console.log(err);
-            const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
+            const message = `Object ${key} is not found in ${bucket}. Check file is exist or bucket is in the same region.`;
             console.log(message);
             callback(message);
-        } else {
-            ses.sendRawEmail({
-                RawMessage: {
-                    Data: data.Body.toString()
-                }
-            }, function(err, data) {
-                if (err)    {
-                    console.log(err, err.stack);
-                    callback(err);
-                }
-                else     {
-                    console.log(data);
-                    callback(null, data.ContentType);
-                }
-            });
-        }
-    });
+        });
 };
