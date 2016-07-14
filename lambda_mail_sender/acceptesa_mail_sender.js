89ffbdce77f2f1dfa32a6454b6f7540b87a1bf64
celeron1ghz <celeron1ghz@gmail.com>
2016-07-14 21:21:44 +0900

    fixed args data structure is wrong

--- lambda_mail_sender/acceptesa_mail_sender.js
+++ lambda_mail_sender/acceptesa_mail_sender.js
@@ -6,8 +6,9 @@ let s3  = new aws.S3({ apiVersion: '2006-03-01' });
 let ses = new aws.SES();
 
 exports.handler = (event, context, callback) => {
-    const bucket = event.Records[0].s3.bucket.name;
-    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    const mess   = JSON.parse(event.Records[0].Sns.Message);
+    const bucket = mess.Records[0].s3.bucket.name;
+    const key    = decodeURIComponent(mess.Records[0].s3.object.key.replace(/\+/g, ' '));
 
     s3.getObject({ Bucket: bucket, Key: key }).promise()
         .then(data => {
