c442e069f2d3a78b30c5ea0717df990e3a215b65
celeron1ghz <celeron1ghz@gmail.com>
2018-04-16 22:10:21 +0900

    formatted

--- handler.js
+++ handler.js
@@ -1,122 +1,122 @@
 'use strict';
 
 const METHODS = {
-    Bounce: e => {
-        let b = e.bounce;
-        let m = e.mail;
-        return {
-            text:        '*BOUNCE MAIL DETECTED!!!!!*',
-            icon_emoji:  ':warning:',
-            attachments: [
-                { color: 'warning', title: "Bounce Type",  text: b.bounceType + ' / ' + b.bounceSubType, },
-                { color: 'warning', title: "ID",           text: m.messageId, },
-                { color: 'warning', title: "From Address", text: m.source, },
-                { color: 'warning', title: "To Address",   text: m.destination.toString(), },
-                { color: 'warning', title: "Send At",      text: m.timestamp, },
-            ],
-        }
-    },
-    Complaint: e => {
-        let c = e.complaint;
-        let m = e.mail;
-        return {
-            text:        '*COMPLAINT MAIL DETECTED!!!!!*',
-            icon_emoji:  ':warning:',
-            attachments: [
-                { color: 'danger', title: "Complaint Type", text: c.complaintFeedbackType, },
-                { color: 'danger', title: "ID",             text: m.messageId, },
-                { color: 'danger', title: "From Address",   text: m.source, },
-                { color: 'danger', title: "To Address",     text: m.destination.toString(), },
-                { color: 'danger', title: "Send At",        text: m.timestamp, },
-            ],
-        };
-    },
-    Delivery: e => {
-        let m = e.mail;
-        return {
-            text:        '',
-            attachments: [{
-                mrkdwn_in: ['text'],
-                color: 'good',
-                title: 'Sending Mail Success',
-                text:  `From = ${m.source}` + "\n" +
-                       `To = [ ${m.destination.toString()} ]` + "\n" +
-                       `ID = ${m.messageId}` + "\n" +
-                       `SendAt = ${m.timestamp}`,
-            }],
-        };
-    },
+  Bounce: e => {
+    let b = e.bounce;
+    let m = e.mail;
+    return {
+      text: '*BOUNCE MAIL DETECTED!!!!!*',
+      icon_emoji: ':warning:',
+      attachments: [
+        { color: 'warning', title: "Bounce Type", text: b.bounceType + ' / ' + b.bounceSubType, },
+        { color: 'warning', title: "ID", text: m.messageId, },
+        { color: 'warning', title: "From Address", text: m.source, },
+        { color: 'warning', title: "To Address", text: m.destination.toString(), },
+        { color: 'warning', title: "Send At", text: m.timestamp, },
+      ],
+    }
+  },
+  Complaint: e => {
+    let c = e.complaint;
+    let m = e.mail;
+    return {
+      text: '*COMPLAINT MAIL DETECTED!!!!!*',
+      icon_emoji: ':warning:',
+      attachments: [
+        { color: 'danger', title: "Complaint Type", text: c.complaintFeedbackType, },
+        { color: 'danger', title: "ID", text: m.messageId, },
+        { color: 'danger', title: "From Address", text: m.source, },
+        { color: 'danger', title: "To Address", text: m.destination.toString(), },
+        { color: 'danger', title: "Send At", text: m.timestamp, },
+      ],
+    };
+  },
+  Delivery: e => {
+    let m = e.mail;
+    return {
+      text: '',
+      attachments: [{
+        mrkdwn_in: ['text'],
+        color: 'good',
+        title: 'Sending Mail Success',
+        text: `From = ${m.source}` + "\n" +
+          `To = [ ${m.destination.toString()} ]` + "\n" +
+          `ID = ${m.messageId}` + "\n" +
+          `SendAt = ${m.timestamp}`,
+      }],
+    };
+  },
 };
 
 
 module.exports.notifier = (event, context, callback) => {
-    const e    = JSON.parse(event.Records[0].Sns.Message);
-    const type = e.notificationType;
-    let ret;
+  const e = JSON.parse(event.Records[0].Sns.Message);
+  const type = e.notificationType;
+  let ret;
 
-    const aws = require('aws-sdk');
-    const vo  = require('vo');
-    const ssm = new aws.SSM({ region: 'ap-northeast-1' });
+  const aws = require('aws-sdk');
+  const vo = require('vo');
+  const ssm = new aws.SSM({ region: 'ap-northeast-1' });
 
-    if (METHODS[type])   {
-        ret = METHODS[type](e);
-    } else {
-        ret = {
-            text:        type,
-            attachments: [{
-                color: 'danger',
-                text:  "UNKNOWN",
-                title: "UNKNOWN",
-            }],
-        };
-    }
+  if (METHODS[type]) {
+    ret = METHODS[type](e);
+  } else {
+    ret = {
+      text: type,
+      attachments: [{
+        color: 'danger',
+        text:  'UNKNOWN',
+        title: 'UNKNOWN',
+      }],
+    };
+  }
 
-    vo(function*(){
-        const url   = (yield ssm.getParameter({ Name: '/s3mail/slack', WithDecryption: true }).promise() ).Parameter.Value;
-        const Slack = require('slack-node');
-        const slack = new Slack();
-        slack.setWebhook(url);
-        ret.mrkdwn  = true;
+  vo(function* () {
+    const url = (yield ssm.getParameter({ Name: '/s3mail/slack', WithDecryption: true }).promise()).Parameter.Value;
+    const Slack = require('slack-node');
+    const slack = new Slack();
+    slack.setWebhook(url);
+    ret.mrkdwn = true;
 
-        const slack_ret = yield new Promise((resolve,reject) => {
-            slack.webhook(ret, (err,res) => {
-                if (err) { reject(err) } else { resolve(res) }
-            });
-        });
-
-        console.log(slack_ret);
-        callback(null, slack_ret);
-    }).catch(err => {
-        console.log("error happen:", err);
-        callback(err);
+    const slack_ret = yield new Promise((resolve, reject) => {
+      slack.webhook(ret, (err, res) => {
+        if (err) { reject(err) } else { resolve(res) }
+      });
     });
+
+    console.log(slack_ret);
+    callback(null, slack_ret);
+  }).catch(err => {
+    console.log("error happen:", err);
+    callback(err);
+  });
 };
 
 module.exports.sender = (event, context, callback) => {
-    const mess   = JSON.parse(event.Records[0].Sns.Message);
-    const bucket = mess.Records[0].s3.bucket.name;
-    const key    = decodeURIComponent(mess.Records[0].s3.object.key.replace(/\+/g, ' '));
+  const mess = JSON.parse(event.Records[0].Sns.Message);
+  const bucket = mess.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(mess.Records[0].s3.object.key.replace(/\+/g, ' '));
 
-    const aws = require('aws-sdk');
-    const s3  = new aws.S3();
-    const ses = new aws.SES();
-    const vo  = require('vo');
+  const aws = require('aws-sdk');
+  const s3  = new aws.S3();
+  const ses = new aws.SES();
+  const vo  = require('vo');
 
-    vo(function*(){
-        console.log(`S3.getObject(${bucket}#${key})`);
-        const received = yield s3.getObject({ Bucket: bucket, Key: key }).promise();
+  vo(function* () {
+    console.log(`S3.getObject(${bucket}#${key})`);
+    const received = yield s3.getObject({ Bucket: bucket, Key: key }).promise();
 
-        console.log(`SES.sendRawEmail(${received.Body.toString().length})`);
-        const mail = yield ses.sendRawEmail({ RawMessage: { Data: received.Body.toString() } }).promise();
+    console.log(`SES.sendRawEmail(${received.Body.toString().length})`);
+    const mail = yield ses.sendRawEmail({ RawMessage: { Data: received.Body.toString() } }).promise();
 
-        console.log(" ==> ", mail);
-        console.log(`S3.deleteObject(${bucket}#${key})`);
-        const deleted = yield s3.deleteObject({ Bucket: bucket, Key: key }).promise();
+    console.log(" ==> ", mail);
+    console.log(`S3.deleteObject(${bucket}#${key})`);
+    const deleted = yield s3.deleteObject({ Bucket: bucket, Key: key }).promise();
 
-        console.log(" ==> ", deleted);
-        callback(null, "OK");
-    }).catch(err => {
-        console.log("error happen:", err);
-        callback(err);
-    });
+    console.log(" ==> ", deleted);
+    callback(null, "OK");
+  }).catch(err => {
+    console.log("error happen:", err);
+    callback(err);
+  });
 };
